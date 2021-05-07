152dab7d46bcb323aa6ecea8b4f17f7cda3421db
zajer <pikus3@list.pl>
2021-05-07 15:50:55 +0200

    Added transformation of bigraph into a bignet form in Bigraph2Visjs view.

--- src/views/Bigraph2Visjs.js
+++ src/views/Bigraph2Visjs.js
@@ -3,7 +3,7 @@ var Converter = require("../models/Converter")
 var vis = require("vis-network")
 require("vis-network/styles/vis-network.min.css")
 let network;
-let bigraph;
+let bigraphInputContent = "Paste bigraph as text here";
 module.exports = {
 	oncreate: () => {
 		let network_container = document.getElementById("network_container");
@@ -18,23 +18,44 @@ module.exports = {
 		network = new vis.Network(network_container, data, network_options);
 	},
 	onupdate: () => {
-		let bigNet = bigraph.to_BigNetwork();
-		let data = bigNet.to_NetworkDatasets();
+		let bigraphAsBignetContainer = document.getElementById("bignet_output");
+		let bignetDownloadButton = document.getElementById("bignet_download_button");
+		let bigraph = Converter.text_2_bigraph(bigraphInputContent); 
+		let bignet = bigraph.to_BigNetwork();
+		let data = bignet.to_NetworkDatasets();
 		network.setData(data);
+		bigraphAsBignetContainer.value = JSON.stringify(bignet);
+		let bignetBlob = new Blob([bigraphAsBignetContainer.value], {type: 'text/plain'});
+		URL.revokeObjectURL(bignetDownloadButton.href);
+		bignetDownloadButton.href = URL.createObjectURL(bignetBlob)
 	},
     view: () => {
-        return m(".pure-g", { style:{ 'padding': ".5em" } } ,[
-			m(".pure-u-2-5", { style: { 'padding': ".5em", 'box-sizing':'border-box', 'border':'1px solid black' } },[
-				m(".pure-u-1",m("label", "Bigraph as text")),
-				m(".pure-u-1",m("textarea[rows=5][cols=60]", { id:"bigraph_input", style: { 'width':'99%', 'resize':'vertical' } }, "Paste bigraph as text here")),
-				m(".pure-u-1-6",m("button", { 
+        return m(".pure-g" ,[
+			m(".pure-u-2-5", { style: { 'padding': ".5em", 'box-sizing':'border-box', 'border':'1px solid black' } },
+				m(".pure-g",[
+				m(".pure-u-1-1",m("label", "Bigraph as text")),
+				m(".pure-u-1-1", { style: { 'padding' : '.7em' } } ,m("textarea[rows=5][cols=60]", 
+					{ id:"bigraph_input", style: { 'width':'99%', 'resize':'none', 'height': '40vh' } }, 
+					bigraphInputContent)),
+				m(".pure-u-2-6",m("button.pure-button .pure-button-primary", { 
+					style: { 'font-size': '70%' },
 					onclick: () => { 
 						let bigraph_as_text_container = document.getElementById("bigraph_input");
-						let input = bigraph_as_text_container.value;
-						bigraph = Converter.text_2_bigraph(input);
+						bigraphInputContent = bigraph_as_text_container.value;
 					}
-				} , "Visualize"))
-			]),
+				} , "Visualize and transform")),
+				m(".pure-u-1-1", { style: { 'padding' : '.7em' } } ,m("textarea[rows=5][cols=60]", { 
+					readonly: true, 
+					id:"bignet_output", 
+					style: { 'width':'99%', 'resize':'none', 'height': '40vh' }
+				}, "Bigraph transformed into BigNet will appear here.")),
+				m(".pure-u-1-1", m("a.pure-button .pure-button-primary", {
+						id: "bignet_download_button",
+						style: { 'font-size': '70%' }
+				 	} ,
+					"Save BigNet to a file")
+				)
+			])),
 			m(".pure-u-3-5",[
 				m("", { id:"network_container", style:{ 'height': '98vh' } },"abc")
 			])
