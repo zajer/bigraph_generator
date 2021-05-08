a7af878b078f57f092a5d69220cbc48c91a69167
zajer <pikus3@list.pl>
2021-05-09 01:52:32 +0200

    Working bigraph construction.

--- src/views/BigNetConstructor.js
+++ src/views/BigNetConstructor.js
@@ -1,36 +1,70 @@
 var m = require("mithril")
 var vis = require("vis-network")
 require("vis-network/styles/vis-network.min.css")
-class ObjectCreator {
+var BigNetwork = require("../models/BigNetwork")
+var bigNet = new BigNetwork();
+
+class NamedObjectCreator {
     constructor(label,addFunc,delFunc){
         this.objectTypeLabel = label;
-        this.addElementFunction = addFunc;
+        this.addElementFunction = addFunc.bind(bigNet);
         this.deletingElementFunction = delFunc;
     }
     view = () => {
         return [
             m("label", this.objectTypeLabel),
             m("br"),
-            m("button.margin1", { onclick: this.addElementFunction } ,"Add"),
+            m("input.margin1", { id:(this.objectTypeLabel+"-name"),  type:"text", size:10, placeholder: "name" } ),
+            m("button.margin1", { 
+                onclick: () => {
+                    let ctrl = document.getElementById(this.objectTypeLabel+"-name").value;
+                    this.addElementFunction(ctrl); 
+                }} ,"Add"),
             m("br"),
-            m("input.margin1", {  type:"number", placeholder: "ID" } ),
+            m("input.margin1", {  id:(this.objectTypeLabel+"-id"), type:"number", placeholder: "ID" } ),
             m("button.margin1", { onclick:this.deletingElementFunction } ,"Delete")
         ]
     }
 }
-class ConnectionCreator {
+
+class NamelessObjectCreator {
     constructor(label,addFunc,delFunc){
-        this.connectionTypeLabel = label;
+        this.objectTypeLabel = label;
         this.addElementFunction = addFunc;
+        this.deleteElementFunction = delFunc;
+    }
+    view = () => {
+        return [
+            m("label", this.objectTypeLabel),
+            m("br"),
+            m("button.margin1", { onclick: this.addElementFunction.bind(bigNet) } ,"Add"),
+            m("br"),
+            m("input.margin1", { id:(this.objectTypeLabel+"-id"),  type:"number", placeholder: "ID" } ),
+            m("button.margin1", { onclick: this.deleteElementFunction } ,"Delete")
+        ]
+    }
+}
+
+class ConnectionCreator {
+    constructor(label,type,addFunc,delFunc){
+        console.log(addFunc);
+        this.connectionTypeLabel = label;
+        this.connectionType = type;
+        this.addElementFunction = addFunc.bind(bigNet);
         this.deletingElementFunction = delFunc;
     }
     view = () => {
         return [
             m("label", this.connectionTypeLabel),
             m("br"),
-            m("input.margin1", {  type:"number", placeholder: "From" } ),
-            m("input.margin1", {  type:"number", placeholder: "To" } ),
-            m("button.margin1", { onclick: this.addElementFunction} ,"Connect"),
+            m("input.margin1", { id:(this.connectionTypeLabel+"-from"), type:"number", placeholder: "From" } ),
+            m("input.margin1", { id:(this.connectionTypeLabel+"-to"), type:"number", placeholder: "To" } ),
+            m("button.margin1", { 
+                onclick: () => {
+                    let from = document.getElementById(this.connectionTypeLabel+"-from").value;
+                    let to = document.getElementById(this.connectionTypeLabel+"-to").value;
+                    this.addElementFunction(from,to,this.connectionType);
+                }} ,"Connect"),
             m("button.margin1", { onclick: this.deletingElementFunction } ,"Delete connection")
         ]
     }
@@ -39,14 +73,14 @@ class ConnectionCreator {
 var bignetCreator = {
     view: () => {
         return m(".pure-g" ,[
-            m(".pure-u-1", m(new ObjectCreator("Roots",undefined,undefined))),
-            m(".pure-u-1", m(new ObjectCreator("Nodes",undefined,undefined))),
-            m(".pure-u-1", m(new ObjectCreator("Sites",undefined,undefined))),
-            m(".pure-u-1", m(new ConnectionCreator("Place graph connections",undefined,undefined) )),
-            m(".pure-u-1", m(new ObjectCreator("Links",undefined,undefined))),
-            m(".pure-u-1", m(new ObjectCreator("Outerfaces",undefined,undefined))),
-            m(".pure-u-1", m(new ObjectCreator("Innerfaces",undefined,undefined))),
-            m(".pure-u-1", m(new ConnectionCreator("Link graph connections",undefined,undefined) )),
+            m(".pure-u-1", m(new NamelessObjectCreator("Roots",bigNet.add_root,undefined))),
+            m(".pure-u-1", m(new NamedObjectCreator("Nodes",bigNet.add_node,undefined))),
+            m(".pure-u-1", m(new NamelessObjectCreator("Sites",bigNet.add_site,undefined))),
+            m(".pure-u-1", m(new ConnectionCreator("Place graph connections",BigNetwork.PlaceGraphConnectionType,bigNet.connect_elements,undefined) )),
+            m(".pure-u-1", m(new NamelessObjectCreator("Links",bigNet.add_link,undefined))),
+            m(".pure-u-1", m(new NamedObjectCreator("Outerfaces",bigNet.add_outerface,undefined))),
+            m(".pure-u-1", m(new NamedObjectCreator("Innerfaces",bigNet.add_innerface,undefined))),
+            m(".pure-u-1", m(new ConnectionCreator("Link graph connections",BigNetwork.LinkGraphConnectionType,bigNet.connect_elements,undefined) )),
         ])
     }
 }
@@ -60,7 +94,7 @@ function loadBigNetFromFile(){
 let network;
 module.exports = {
     oncreate: () => {
-		let network_container = document.getElementById("network_container");
+		let networkContainer = document.getElementById("network_container");
 		let data = {
 		  nodes: [],
 		  edges: [],
@@ -69,15 +103,21 @@ module.exports = {
 			{
 			height: '100%',
 		  };
-		network = new vis.Network(network_container, data, network_options);
+		network = new vis.Network(networkContainer, data, network_options);
 	},
+    onupdate: () => {
+        let networkContainer = document.getElementById("network_container");
+        let data = bigNet.to_NetworkDatasets(networkContainer);
+        network.setData(data);
+    },
     view: () => {
         return m(".pure-g" ,[
             m(".pure-u-2-5", { style: { height: '100vh', 'padding': ".5em", 'box-sizing':'border-box', 'border':'1px solid black' } }, [
                 m(bignetCreator),
                 m("hr", {width: "100%"}),
-                m(".pure-button .margin1", { onclick: saveBigNetToFile } ,"Save BigNet"),
-                m(".pure-button .margin1", { onclick: loadBigNetFromFile } ,"Load BigNet"),
+                m(".pure-button .pure-button-primary .margin1", { onclick: saveBigNetToFile } ,"Save BigNet"),
+                m(".pure-button .pure-button-primary .margin1", { onclick: loadBigNetFromFile } ,"Load BigNet"),
+                m(".pure-button .pure-button-primary .margin1", { onclick: loadBigNetFromFile } ,"Save BigNet as bigraph")
             ]),
             m(".pure-u-3-5", m("", { id:"network_container", style:{ 'height': '100vh' } },"Here should be displayed a network."))
         ]);
