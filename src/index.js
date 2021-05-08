0bf8601da5b9c034cef1353dc77096c65611df9d
zajer <pikus3@list.pl>
2021-05-08 03:54:08 +0200

    Renamed Bigraph2Visjs to BigraphTransformer.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var m = require("mithril");
 var purecss = require("purecss");
-var Bigraph2Visjs = require("./views/Bigraph2Visjs");
+var Bigraph2Visjs = require("./views/BigraphTransformer");
 var BigNetConstructor = require("./views/BigNetConstructor");
 var Main = require("./views/Main");
 require("./index.css");
